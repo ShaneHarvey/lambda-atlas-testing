c6fe48d6986f9c00621e47e7b96b8a052d2fa406
Shane Harvey <shnhrv@gmail.com>
2023-09-21 16:36:20 -0700

    Working build-and-deploy.sh

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-FUNCTION_NAME=${FUNCTION_NAME:-python-atlas-test}
+STACK_NAME=${STACK_NAME:-python-atlas-test}
 AWS_REGION=${AWS_REGION:-us-east-1}
 
 if [ -z "$MONGODB_URI" ]
@@ -14,10 +14,40 @@ sam build
 
 echo "Deploying Lambda function..."
 sam deploy \
-  --stack-name "${FUNCTION_NAME}" \
+  --stack-name "${STACK_NAME}" \
   --capabilities CAPABILITY_IAM \
   --resolve-s3 \
   --parameter-overrides "MongoDbUri=${MONGODB_URI}" \
   --region "${AWS_REGION}"
 
-sam delete --stack-name ${FUNCTION_NAME} --no-prompts --region "${AWS_REGION}"
+#echo "Getting Lambda function URL..."
+LAMBDA_FUNCTION_ARN=$(sam list stack-outputs \
+  --stack-name ${STACK_NAME} \
+  --region ${AWS_REGION} \
+  --output json | jq '.[] | select(.OutputKey == "MongoDBFunction") | .OutputValue' | tr -d '"'
+)
+echo "Lambda function ARN: $LAMBDA_FUNCTION_ARN"
+export LAMBDA_FUNCTION_ARN=$LAMBDA_FUNCTION_ARN
+
+echo "Getting Lambda function URL..."
+LAMBDA_FUNCTION_URL=$(aws lambda get-function-url-config \
+   --function-name "${LAMBDA_FUNCTION_ARN}" \
+   --region "${AWS_REGION}" \
+   --output json | jq '.FunctionUrl' | tr -d '"'
+)
+echo "Lambda function URL: $LAMBDA_FUNCTION_URL"
+export LAMBDA_FUNCTION_URL=$LAMBDA_FUNCTION_URL
+
+check_lambda_output () {
+  if grep -q FunctionError output.json
+  then
+      echo "Exiting due to FunctionError!"
+      exit 1
+  fi
+  cat output.json | jq -r '.LogResult' | base64 --decode
+}
+
+aws lambda invoke --function-name "${LAMBDA_FUNCTION_ARN}" --region "${AWS_REGION}" --log-type Tail lambda-invoke-standard.json > output.json
+check_lambda_output
+
+#sam delete --stack-name ${STACK_NAME} --no-prompts --region "${AWS_REGION}"
